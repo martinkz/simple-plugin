dce03ab85f51e57df3e35fd76747ec84e508f88c
Martin K <martinkz@users.noreply.github.com>
2023-02-07 18:31:17 +0000

    Simplify demo

--- dist/simple-demo-component.mjs
+++ dist/simple-demo-component.mjs
@@ -1,18 +1,18 @@
-import { ref as l, openBlock as m, createElementBlock as p, Fragment as r, createElementVNode as o, toDisplayString as c } from "vue";
-const u = {
+import { ref as l, openBlock as m, createElementBlock as p, Fragment as r, createElementVNode as o, toDisplayString as u } from "vue";
+const c = {
   __name: "SimpleDemoComponent",
   setup(e) {
     const n = l(0);
     return (i, t) => (m(), p(r, null, [
-      o("h1", null, c(n.value), 1),
+      o("h1", null, u(n.value), 1),
       o("button", {
-        onClick: t[0] || (t[0] = (s) => n.value++)
+        onClick: t[0] || (t[0] = (a) => n.value++)
       }, "Add 1")
     ], 64));
   }
 };
 function _(e) {
-  e.component("SimpleDemoComponent", u);
+  e.component("SimpleDemoComponent", c);
 }
 export {
   _ as SimpleTestPlugin
--- src/SimpleTestPlugin/index.js
+++ src/SimpleTestPlugin/index.js
@@ -1,5 +1,4 @@
 import SimpleDemoComponent from "./SimpleDemoComponent.vue";
-import "./assets/style.css";
 
 export function SimpleTestPlugin(app) {
 	app.component("SimpleDemoComponent", SimpleDemoComponent);
